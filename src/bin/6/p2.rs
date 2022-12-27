8eaa7ab5bf247b65e76e330ed0e7056d4ee3864e
thebenkogan <benkogan9@gmail.com>
2022-12-27 04:05:25 -0500

    day 6

---
+++ src/bin/6/p2.rs
@@ -0,0 +1,24 @@
+use std::collections::VecDeque;
+
+use itertools::Itertools;
+
+const WINDOW_SIZE: usize = 14;
+
+pub fn solve(input: &str) -> i32 {
+    let mut window: VecDeque<char> = VecDeque::with_capacity(WINDOW_SIZE);
+    for (i, c) in input.chars().enumerate() {
+        if window.len() < WINDOW_SIZE {
+            window.push_front(c);
+            continue;
+        }
+
+        if window.iter().unique().count() == window.len() {
+            return i as i32;
+        }
+
+        window.pop_back();
+        window.push_front(c);
+    }
+
+    unreachable!()
+}
